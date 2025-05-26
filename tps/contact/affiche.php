9b88de7be533cc1a67fd828754d6918dbd601c13
3azzyy <adamsouhailbida@gmail.com>
2025-05-26 15:55:36 +0100

    .....

--- tps/contact/affiche.php
+++ tps/contact/affiche.php
@@ -1,122 +1,46 @@
+<?php include "dblogin.php";
+$conn = connexion();
+?>
+
 <h2>Répertoire de contacts</h2>
 <form method="post">
     <input type="text" name="search">
-    <button type="submit" name="se">chercher</button>
+    <button type="submit" name="se" value="ch">chercher</button>
 </form>
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Prénom</th>
-        <th>Nom</th>
-        <th>Email</th>
-        <th>Téléphone</th>
-        <th>Actions</th>
-    </tr>
-
-<?php
-
-$username = 'souhail';
-$db = 'localhost';
-$password = 'souhail123';
-
-$conn = new PDO("mysql:host=$db;dbname=tps",$username,$password);
-    if($_POST!=NULL){
-    if($_POST['search']===""){
-    $query = $conn->prepare("SELECT * from contact");
-    $query -> execute();
-    $res = $query ->fetchall();
-   
-    }
-    if($_POST['search']!==""){
-    $query = $conn->prepare("SELECT * from contact WHERE nom LIKE :nom or email like :email");
-    $query->bindParam(':nom',$nm);
-    $query->bindParam(':email',$nm);
-    $nm = "%".$_POST['search']."%";
-    $query -> execute();
-    $res = $query ->fetchall();
-   
-    }}
- 
-
-    
-    
-    
-    foreach ($res as $c){ 
-       ?>
-    <tr>
-        <td><?= $c['id'] ?></td>
-        <td><?= $c['prenom'] ?></td>
-        <td><?= $c['nom'] ?></td>
-        <td><?= $c['email'] ?></td>
-        <td><?= $c['telephone'] ?></td>
-        <td>
-            <form method="get">
-            <button type="submit" name="m" value=<?php echo $c['id']; ?>>modifier</button>
-            <button type="submit" name="p" value=<?php echo $c['id']; ?>>supprimer</button>
-            </form>
-        </td>
-    </tr>
-    <?php  
-    }
-    ?>
-</table>
-
-<?php
-
-        if($_GET['p']!=NULL){
-        $query = $conn->prepare("DELETE FROM contact WHERE id=:id;");
-        $query->bindParam(':id',$_GET['p']);
-        $query->execute();
-        $_GET = NULL;
-        header("location:affiche.php");
-        }
-
-
-        if($_GET['m']!=NULL){
-            $id = $_GET['m'];
-            $_GET=NULL;
-            $query = $conn->prepare("SELECT * FROM contact WHERE id=:id;");
-            $query->bindParam(':id',$id);
-            $query->execute();
-            $res = $query->fetch();
-      
-        ?>
-
 <form method="post">
-    <input name="prenom"               value=<?php echo $res['prenom']?>>
-    <input name="nom"                  value=<?php echo $res['nom']?>>
-    <input name="email" type="email"   value=<?php echo $res['email']?>>
-    <input name="telephone"            value=<?php echo $res['telephone']?>>
-    <button type="submit" name="j" value="l">mise a jour</button>
+    <button type="submit" name="tt" value="kk">afficher tout les contacts</button>
+</form>
+<form method="post">
+    <button type="submit" name="aj" value="ac">ajouter contact</button>
 </form>
 
 <?php
+if($_POST!=NULL){
+    $tst = $_POST;
+    
+    if(in_array("kk",$tst)){
+    ttafficher($conn);
+    $tst = NULL;}
 
-    if($_POST!=NULL){
-    
-    
-    $query = $conn->prepare("UPDATE CONTACT SET prenom=:prenom ,nom=:nom ,email=:email ,telephone=:telephone WHERE id=:id;");
-    $query->bindParam(':prenom',$name1);
-    $query->bindParam(':nom',$name2);
-    $query->bindParam(':email',$email);
-    $query->bindParam(':telephone',$phone);
-    $query->bindParam(':id',$id);
-    $name1 = $_POST['prenom'];
-    $name2 = $_POST['nom'];
-    $email = $_POST['email'];
-    $phone = $_POST['telephone'];
-    echo $name1; 
-    echo $name2;
-    echo $email;
-    echo $phone;
-    echo $id;
-    $query->execute();
-    header("location:affiche.php");
-    
-
-  
-   
+    else if(in_array("ch",$tst)){
+    search($conn,$tst['search']);
+    $tst = NULL;    
     }
-     }
+
+    else if(in_array("ac",$tst)){
+    add($conn);
+    $tst = NULL;    
+    }}
+
+if($_GET!=NULL){
+     if(array_key_exists("p",$_GET)){
+    delete($conn,$_GET['p']);
+    $_GET = NULL;
+    }
+
+    else if(array_key_exists("m",$_GET)){
+    mod($conn,$_GET['m']);
+    $_GET = NULL;
+    }}
  ?>
  
---
+++ tps/contact/dblogin.php
@@ -0,0 +1,139 @@
+<?php
+function connexion(){
+$username = 'souhail';
+$db = 'localhost';
+$password = 'souhail123';
+return new PDO("mysql:host=$db;dbname=tps",$username,$password);
+}
+
+function ttafficher($conn){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Prénom</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Téléphone</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $query = $conn->prepare("SELECT * from contact");
+    $query -> execute();
+    printt($query ->fetchall());
+}
+
+function search($conn,$post)
+{   if($post != ""){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Prénom</th>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Téléphone</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $nm = "%".$post."%";
+    $query = $conn->prepare("SELECT * from contact WHERE nom LIKE :nom or email like :email");
+    $query->bindParam(':nom',$nm);
+    $query->bindParam(':email',$nm);
+    $query -> execute();
+    printt( $query ->fetchall());}
+    
+}
+
+function printt($res)
+{
+    foreach ($res as $c){ 
+       ?>
+    <tr>
+        <td><?= $c['id'] ?></td>
+        <td><?= $c['prenom'] ?></td>
+        <td><?= $c['nom'] ?></td>
+        <td><?= $c['email'] ?></td>
+        <td><?= $c['telephone'] ?></td>
+        <td>
+            <form method="get">
+            <button type="submit" name="m" value=<?php echo $c['id']; ?>>modifier</button>
+            </form>
+            <form method="get">
+            <button type="submit" name="p" value=<?php echo $c['id']; ?>>supprimer</button>
+            </form>
+        </td>
+    </tr>
+    <?php  
+    }
+    ?>
+</table>
+<?php
+}
+
+function delete($conn,$id){
+    $query = $conn->prepare("DELETE FROM contact WHERE id=:id;");
+    $query->bindParam(':id',$id);
+    $query->execute();
+}
+
+function mod($conn,$id){
+        $query = $conn->prepare("SELECT * FROM contact WHERE id=:id;");
+        $query->bindParam(':id',$id);
+        $query->execute();
+        $res = $query->fetch();
+?>
+
+<form method="post">
+    <input name="prenom"               value=<?php echo $res['prenom']?>>
+    <input name="nom"                  value=<?php echo $res['nom']?>>
+    <input name="email" type="text"   value=<?php echo $res['email']?>>
+    <input name="telephone"            value=<?php echo $res['telephone']?>>
+    <button type="submit" name="j" value="maj">mise a jour</button>
+</form>
+
+<?php
+    if(in_array("maj",$_POST)){
+    $name1 = $_POST['prenom'];
+    $name2 = $_POST['nom'];
+    $email = $_POST['email'];
+    $phone = $_POST['telephone'];
+    $query = $conn->prepare("UPDATE CONTACT SET prenom=:prenom ,nom=:nom ,email=:email ,telephone=:telephone WHERE id=:id;");
+    $query->bindParam(':prenom',$name1);
+    $query->bindParam(':nom',$name2);
+    $query->bindParam(':email',$email);
+    $query->bindParam(':telephone',$phone);
+    $query->bindParam(':id',$id); 
+    $query->execute();
+    header("location:affiche.php");
+    }
+     }
+
+function add($conn){
+    
+?> 
+<form method="post">
+    <input name="prenom" placeholder="Prénom" required>
+    <input name="nom" placeholder="Nom" required>
+    <input name="email" type="text" placeholder="Email" required>
+    <input name="telephone" placeholder="Téléphone" required>
+    <button type="submit" name="addd" value="ad">Ajouter</button>
+</form>
+
+<?php
+$name1 = $_POST['prenom'];
+$name2 = $_POST['nom'];
+$email = $_POST['email'];
+$phone = $_POST['telephone'];
+$query = $conn->prepare("INSERT INTO CONTACT (prenom,nom,email,telephone)  values (:prenom,:nom,:email,:telephone);");
+$query->bindParam(':prenom',$name1);
+$query->bindParam(':nom',$name2);
+$query->bindParam(':email',$email);
+$query->bindParam(':telephone',$phone);
+echo $query->execute();
+    
+    
+
+}
+
+?>
