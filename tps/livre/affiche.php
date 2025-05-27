ef27857dee7b6ca2e1fe57a84a52106e9aa068f2
3azzyy <adamsouhailbida@gmail.com>
2025-05-27 01:22:22 +0100

    livres done

--- tps/livre/affiche.php
+++ tps/livre/affiche.php
@@ -1,26 +1,52 @@
 <?php include "dblogin.php";
-      include "add.php";
 $conn = connexion();
 ?>
 
-<h2>Répertoire de contacts</h2>
+<h2>gestion de livre</h2>
+
 <form method="post">
     <input type="text" name="search">
     <button type="submit" name="se" value="ch">chercher</button>
 </form>
+
 <form method="post">
-    <button type="submit" name="tt" value="kk">afficher tout les contacts</button>
+    <button type="submit" name="tt" value="kk">afficher tout les livres</button>
 </form>
+
 <form method="post">
-    <button type="submit" name="aj" value="ac">ajouter contact</button>
+    <button type="submit" name="tt1" value="kk1">afficher tout les livres par date</button>
 </form>
+
+<form method="post">
+    <button type="submit" name="tt2" value="kk2">afficher tout les livres par auteur</button>
+</form>
+
+<form method="post">
+    <button type="submit" name="aj" value="ac">ajouter un livre</button>
+</form>
+
+<?php
+    drop($conn);
+?>
+
+
+
 
 <?php
 if($_POST!=NULL){
     $tst = $_POST;
     
+
     if(in_array("kk",$tst)){
     ttafficher($conn);
+    $tst = NULL;}
+
+    else if(in_array("kk1",$tst)){
+    ttafficherdate($conn);
+    $tst = NULL;}
+
+    else if(in_array("kk2",$tst)){
+    ttafficherauteur($conn);
     $tst = NULL;}
         
     else if(in_array("ch",$tst)){
@@ -29,37 +55,50 @@ if($_POST!=NULL){
     }
 
     else if(in_array("ac",$tst)){
-    add($conn);
-    var_dump($_POST);
+    add();
     }
 
-    else if(in_array("ad",$tst)){
-    $name1 = $_POST['prenom'];
-    $name2 = $_POST['nom'];
-    $email = $_POST['email'];
-    $phone = $_POST['telephone'];
-    $query = $conn->prepare("INSERT INTO CONTACT (prenom, nom, email, telephone) VALUES (:prenom, :nom, :email, :telephone)");
-    $query->bindParam(':prenom', $name1);
-    $query->bindParam(':nom', $name2);
-    $query->bindParam(':email', $email);
-    $query->bindParam(':telephone', $phone);
+    else if(in_array("ad",$_POST)){
+    $name1 = $_POST['titre'];
+    $name2 = $_POST['auteur'];
+    $email = $_POST['datepub'];
+    $phone = $_POST['genre'];
+    $query = $conn->prepare("INSERT INTO livre (titre, auteur, datepub, genre) VALUES (:titre, :auteur, :datepub, :genre);");
+    $query->bindParam(':titre', $name1);
+    $query->bindParam(':auteur', $name2);
+    $query->bindParam(':datepub', $email);
+    $query->bindParam(':genre', $phone);
     $query->execute();
+    $tst = NULL;  
     }
+
+    else if(array_key_exists("p",$_POST)){
+    delete($conn,$_POST['p']);
+    $_POST = NULL;
+    }
+
+    else if(array_key_exists("m",$_POST)){
+    mod($conn,$_POST['m']);
+    $_POST = NULL;
+    }
+
+    else if(in_array("maj",$_POST)){
+    $name1 = $_POST['titre'];
+    $name2 = $_POST['auteur'];
+    $email = $_POST['datepub'];
+    $phone = $_POST['genre'];
+    $id = $_POST['id'];
+    $query = $conn->prepare("UPDATE livre SET titre=:titre ,auteur=:auteur ,datepub=:datepub ,genre=:genre WHERE id=:id;");
+    $query->bindParam(':titre',$name1);
+    $query->bindParam(':auteur',$name2);
+    $query->bindParam(':datepub',$email);
+    $query->bindParam(':genre',$phone);
+    $query->bindParam(':id',$id); 
+    $query->execute();
     
-    $tst = NULL;    
-    }
-    
-
-
-if($_GET!=NULL){
-     if(array_key_exists("p",$_GET)){
-    delete($conn,$_GET['p']);
-    $_GET = NULL;
     }
 
-    else if(array_key_exists("m",$_GET)){
-    mod($conn,$_GET['m']);
-    $_GET = NULL;
-    }}
- ?>
- 
+    else if(in_array("drop",$_POST)){
+        genre($conn,$_POST['genres']);
+    }
+} 
--- tps/livre/dblogin.php
+++ tps/livre/dblogin.php
@@ -13,12 +13,46 @@ function ttafficher($conn){
         <th>ID</th>
         <th>titre</th>
         <th>auteur</th>
-        <th>annees pub</th>
-        <th>Téléphone</th>
+        <th>date de pub</th>
         <th>genre</th>
+        <th>Actions</th>
     </tr>
     <?php
     $query = $conn->prepare("SELECT * from livre");
+    $query -> execute();
+    printt($query ->fetchall());
+}
+
+function ttafficherdate($conn){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>titre</th>
+        <th>auteur</th>
+        <th>date de pub</th>
+        <th>genre</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $query = $conn->prepare("SELECT * from livre ORDER BY datepub");
+    $query -> execute();
+    printt($query ->fetchall());
+}
+
+function ttafficherauteur($conn){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>titre</th>
+        <th>auteur</th>
+        <th>date de pub</th>
+        <th>genre</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $query = $conn->prepare("SELECT * from livre ORDER BY auteur");
     $query -> execute();
     printt($query ->fetchall());
 }
@@ -29,17 +63,16 @@ function search($conn,$post)
     <table border="1">
     <tr>
         <th>ID</th>
-        <th>Prénom</th>
-        <th>Nom</th>
-        <th>Email</th>
-        <th>Téléphone</th>
+        <th>titre</th>
+        <th>auteur</th>
+        <th>date de pub</th>
+        <th>genre</th>
         <th>Actions</th>
     </tr>
     <?php
     $nm = "%".$post."%";
-    $query = $conn->prepare("SELECT * from livre WHERE nom LIKE :nom or email like :email");
-    $query->bindParam(':nom',$nm);
-    $query->bindParam(':email',$nm);
+    $query = $conn->prepare("SELECT * from livre WHERE titre LIKE :titre ");
+    $query->bindParam(':titre',$nm);
     $query -> execute();
     printt( $query ->fetchall());}
     
@@ -51,15 +84,15 @@ function printt($res)
        ?>
     <tr>
         <td><?= $c['id'] ?></td>
-        <td><?= $c['prenom'] ?></td>
-        <td><?= $c['nom'] ?></td>
-        <td><?= $c['email'] ?></td>
-        <td><?= $c['telephone'] ?></td>
+        <td><?= $c['titre'] ?></td>
+        <td><?= $c['auteur'] ?></td>
+        <td><?= $c['datepub'] ?></td>
+        <td><?= $c['genre'] ?></td>
         <td>
-            <form method="get">
+            <form method="post">
             <button type="submit" name="m" value=<?php echo $c['id']; ?>>modifier</button>
             </form>
-            <form method="get">
+            <form method="post">
             <button type="submit" name="p" value=<?php echo $c['id']; ?>>supprimer</button>
             </form>
         </td>
@@ -85,54 +118,67 @@ function mod($conn,$id){
 ?>
 
 <form method="post">
-    <input name="prenom"               value=<?php echo $res['prenom']?>>
-    <input name="nom"                  value=<?php echo $res['nom']?>>
-    <input name="email" type="text"   value=<?php echo $res['email']?>>
-    <input name="telephone"            value=<?php echo $res['telephone']?>>
+    <input name="id"   hidden       value=<?php echo $res['id']?>>
+    <input name="titre"               value=<?php echo $res['titre']?>>
+    <input name="auteur"                  value=<?php echo $res['auteur']?>>
+    <input name="datepub" type="number"   value=<?php echo $res['datepub']?>>
+    <input name="genre"            value=<?php echo $res['genre']?>>
     <button type="submit" name="j" value="maj">mise a jour</button>
 </form>
 
 <?php
-    if(in_array("maj",$_POST)){
-    $name1 = $_POST['prenom'];
-    $name2 = $_POST['nom'];
-    $email = $_POST['email'];
-    $phone = $_POST['telephone'];
-    $query = $conn->prepare("UPDATE livre SET titre=:prenom ,auteur=:nom ,datepub=:email ,genre=:telephone WHERE id=:id;");
-    $query->bindParam(':prenom',$name1);
-    $query->bindParam(':nom',$name2);
-    $query->bindParam(':email',$email);
-    $query->bindParam(':telephone',$phone);
-    $query->bindParam(':id',$id); 
-    $query->execute();
-    header("location:affiche.php");
-    }
-     }
-
-function add($conn){
     
+}
+
+function add(){  
 ?> 
 <form method="post">
-    <input name="prenom" placeholder="Prénom" required>
-    <input name="nom" placeholder="Nom" required>
-    <input name="email" type="text" placeholder="Email" required>
-    <input name="telephone" placeholder="Téléphone" required>
+    <input name="titre" placeholder="titre" required>
+    <input name="auteur" placeholder="auteur" required>
+    <input name="datepub" type="number" placeholder="datepub" required>
+    <input name="genre" placeholder="genre" required>
     <button type="submit" name="addd" value="ad">Ajouter</button>
 </form>
-
 <?php
-$name1 = $_POST['prenom'];
-$name2 = $_POST['nom'];
-$email = $_POST['email'];
-$phone = $_POST['telephone'];
-$query = $conn->prepare("INSERT INTO CONTACT (titre,auteur,datepub,genre)  values (:prenom,:nom,:email,:telephone);");
-$query->bindParam(':prenom',$name1);
-$query->bindParam(':nom',$name2);
-$query->bindParam(':email',$email);
-$query->bindParam(':telephone',$phone);
-echo $query->execute();
-    
-    
 
 }
+
+function drop($conn){
+?>  
+    <form method="post">
+    <label for="genres">Genre de livre:</label>
+    <select name="genres">
+<?php
+    $query = $conn -> prepare("SELECT DISTINCT genre FROM livre ORDER BY genre;");
+    $query->execute();
+    $res = $query->fetchAll();
+    foreach ($res as $re){
 ?>
+    <option value=<?php echo $re[0] ?>><?php echo $re[0] ?></option>
+<?php
+    }
+?>
+    </select>
+    <button type="submit" name="drop" value="drop">chercher</button>
+    </form>
+<?php
+
+}
+
+function genre($conn,$genres){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>titre</th>
+        <th>auteur</th>
+        <th>date de pub</th>
+        <th>genre</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $query = $conn -> prepare("SELECT * FROM livre where genre=:genre ORDER BY titre;");
+    $query->bindParam(":genre",$genres);
+    $query->execute();
+    printt($query->fetchAll());
+}
