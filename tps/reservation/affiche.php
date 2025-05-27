ad2380ef78de298a598e89232faf0c66c9c85908
3azzyy <adamsouhailbida@gmail.com>
2025-05-27 02:20:43 +0100

    RESERVATION (mod not working )

---
+++ tps/reservation/affiche.php
@@ -0,0 +1,76 @@
+<?php include "dblogin.php";
+$conn = connexion();
+?>
+
+<h2>gestion de reservation</h2>
+
+<form method="post">
+    <button type="submit" name="tt" value="kk">afficher tout les reservations</button>
+</form>
+
+<form method="post">
+    <button type="submit" name="aj" value="ac">ajouter une reservation</button>
+</form>
+
+
+
+
+<?php
+if($_POST!=NULL){
+    $tst = $_POST;
+    
+    if(in_array("kk",$tst)){
+    ttafficher($conn);
+    $tst = NULL;}
+
+    else if(in_array("ac",$tst)){
+    add();
+    }
+
+    else if(in_array("ad",$_POST)){
+    $name1 = $_POST['nom'];
+    $name2 = $_POST['date'];
+    $email = $_POST['debut'];
+    $phone = $_POST['fin'];
+    $desc = $_POST['description'];
+    $query = $conn->prepare("INSERT INTO reservation (nom, date, debut, fin,description) VALUES (:nom, :date, :debut, :fin,:description);");
+    $query->bindParam(':nom', $name1);
+    $query->bindParam(':date', $name2);
+    $query->bindParam(':debut', $email);
+    $query->bindParam(':fin', $phone);
+    $query->bindParam(':description', $desc);
+    $query->execute();
+    $tst = NULL;  
+    }
+
+    else if(array_key_exists("p",$_POST)){
+    delete($conn,$_POST['p']);
+    $_POST = NULL;
+    }
+
+    else if(array_key_exists("m",$_POST)){
+    mod($conn,$_POST['m']);
+    $_POST = NULL;
+    }
+
+    else if(in_array("maj",$_POST)){
+    $name1 = $_POST['nom'];
+    $name2 = $_POST['date'];
+    $email = $_POST['debut'];
+    $phone = $_POST['fin'];
+    $desc = $_POST['description'];
+    $id = $_POST['id'];
+    $query = $conn->prepare("UPDATE reservation SET nom=:nom ,date=:datee ,debut=:debut ,fin=:fin , description=:descritpion  WHERE id=:id;");
+    $query->bindParam(':nom', $name1);
+    $query->bindParam(':datee', $name2);
+    $query->bindParam(':debut', $email);
+    $query->bindParam(':fin', $phone);
+    $query->bindParam(':description', $desc);
+    $query->bindParam(':id',$id); 
+    var_dump($query);
+    $query->execute();
+    
+    }
+
+    
+} 
---
+++ tps/reservation/dblogin.php
@@ -0,0 +1,96 @@
+<?php
+function connexion(){
+$username = 'souhail';
+$db = 'localhost';
+$password = 'souhail123';
+return new PDO("mysql:host=$db;dbname=tps",$username,$password);
+}
+
+function ttafficher($conn){
+    ?>
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>user</th>
+        <th>date res</th>
+        <th>heure debut</th>
+        <th>heure fin</th>
+        <th>description</th>
+        <th>Actions</th>
+    </tr>
+    <?php
+    $query = $conn->prepare("SELECT * from reservation");
+    $query -> execute();
+    printt($query ->fetchall());
+}
+
+function printt($res)
+{
+    foreach ($res as $c){ 
+       ?>
+    <tr>
+        <td><?= $c['id'] ?></td>
+        <td><?= $c['nom'] ?></td>
+        <td><?= $c['date'] ?></td>
+        <td><?= $c['debut'] ?></td>
+        <td><?= $c['fin'] ?></td>
+        <td><?= $c['description'] ?></td>
+        <td>
+            <form method="post">
+            <button type="submit" name="m" value=<?php echo $c['id']; ?>>modifier</button>
+            </form>
+            <form method="post">
+            <button type="submit" name="p" value=<?php echo $c['id']; ?>>supprimer</button>
+            </form>
+        </td>
+    </tr>
+    <?php  
+    }
+    ?>
+</table>
+<?php
+}
+
+function delete($conn,$id){
+    $query = $conn->prepare("DELETE FROM reservation WHERE id=:id;");
+    $query->bindParam(':id',$id);
+    $query->execute();
+}
+
+function mod($conn,$id){
+        $query = $conn->prepare("SELECT * FROM reservation WHERE id=:id;");
+        $query->bindParam(':id',$id);
+        $query->execute();
+        $res = $query->fetch();
+?>
+
+<form method="post">
+    <input name="id" hidden  value=<?php echo $res['id']?>>
+    <input name="nom" type="text" value=<?php echo $res['nom']?>>
+    <input name="date" type="date" value=<?php echo $res['date']?>>
+    <input name="debut" type="time" value=<?php echo $res['debut']?>>
+    <input name="fin" type="time" value=<?php echo $res['fin']?>>
+    <input name="description"  type="textaera" value=<?php echo $res['description']?>>
+    <button type="submit" name="j" value="maj">mise a jour</button>
+</form>
+
+<?php
+    
+}
+
+function add(){  
+?> 
+<form method="post">
+    <input name="nom" type="text" placeholder="nom" required>
+    <input name="date" type="date" placeholder="date" required>
+    <input name="debut" type="time" placeholder="debut" required>
+    <input name="fin" type="time" placeholder="fin" required>
+    <input name="description" type="textaera" placeholder="description" required>
+    <button type="submit" name="addd" value="ad">Ajouter</button>
+</form>
+<?php
+
+}
+
+
+
